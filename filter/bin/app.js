0b721a83c688462cc4f72fcd4c54d8de86a372e0
phiresky <phireskyde+git@gmail.com>
2016-05-19 19:52:31 +0200

    update binaries

--- filter/bin/app.js
+++ filter/bin/app.js
@@ -1,17 +1,21 @@
-System.register(['react', 'react-dom', 'react-select', 'query-string', "reactable"], function(exports_1, context_1) {
+System.register(['react', 'react-dom', 'react-select', 'query-string'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
-    var React, ReactDOM, react_select_1, query_string_1, Reactable;
-    var Table, Tr, Td, Thead, Th, users, idToName, nameToId, GameRender, defaultState, GUI;
-    function getUnique(data, attribute) {
-        return [...new Set(Object.keys(data).map(k => data[k]).map(game => game[attribute]))];
+    var React, ReactDOM, react_select_1, query_string_1;
+    var users, idToName, GUI;
+    function renderTableRow(data, id) {
+        const hrefs = data.ranking.map(id => React.createElement("a", {href: `https://codeit.itdhosting.de:1337/profile.php?id=${id}`}, idToName.get(id)));
+        return React.createElement("tr", {key: id}, [id, data.date, data.map, data.mapSize, hrefs[0], data.resources[0], hrefs[1], data.resources[1],
+            React.createElement("a", {href: `https://codeit.itdhosting.de:1337/replay.php?game=${id}`}, "Show Replay")
+        ].map(x => React.createElement("td", null, x)));
     }
     function serializeState(state) {
         const obj = {
             map: state.mapFilter && state.mapFilter.map(x => x.value).join("."),
             players: state.playerFilter && state.playerFilter.map(x => x.value).join("."),
             winner: state.winnerFilter && state.winnerFilter.value,
-            looser: state.looserFilter && state.looserFilter.value
+            looser: state.looserFilter && state.looserFilter.value,
+            sort: state.sortColumn, reverse: state.sortReverse
         };
         for (const x in obj)
             if (!obj[x])
@@ -22,11 +26,12 @@ System.register(['react', 'react-dom', 'react-select', 'query-string', "reactabl
     function deserializeState(state) {
         const obj = query_string_1.default.parse(state);
         return {
-            displayLimit: 200,
+            displayLimit: 100,
             mapFilter: obj.map ? obj.map.split(".").map((x) => ({ value: x, label: x })) : null,
             playerFilter: obj.players ? obj.players.split(".").map((x) => ({ value: +x, label: idToName.get(+x) })) : null,
             winnerFilter: obj.winner ? { value: +obj.winner, label: idToName.get(+obj.winner) } : null,
-            looserFilter: obj.looser ? { value: +obj.looser, label: idToName.get(+obj.looser) } : null
+            looserFilter: obj.looser ? { value: +obj.looser, label: idToName.get(+obj.looser) } : null,
+            sortColumn: obj.sort || "ID", sortReverse: typeof obj.reverse === "undefined" ? true : !!obj.reverse,
         };
     }
     function render(stuff) {
@@ -51,58 +56,67 @@ System.register(['react', 'react-dom', 'react-select', 'query-string', "reactabl
             },
             function (query_string_1_1) {
                 query_string_1 = query_string_1_1;
-            },
-            function (Reactable_1) {
-                Reactable = Reactable_1;
             }],
         execute: function() {
-            Table = Reactable.Table, Tr = Reactable.Tr, Td = Reactable.Td, Thead = Reactable.Thead, Th = Reactable.Th;
-            idToName = new Map(), nameToId = new Map();
-            GameRender = class GameRender {
-                static get(data, id) {
-                    const hrefs = data.ranking.map(id => React.createElement("a", {href: `https://codeit.itdhosting.de:1337/profile.php?id=${id}`}, idToName.get(id)));
-                    return React.createElement(Tr, {key: id}, 
-                        React.createElement(Td, {column: "ID"}, id), 
-                        React.createElement(Td, {column: "Date"}, data.date), 
-                        React.createElement(Td, {column: "Map"}, data.map), 
-                        React.createElement(Td, {column: "Map Size"}, data.mapSize), 
-                        React.createElement(Td, {column: "Winner"}, hrefs[0]), 
-                        React.createElement(Td, {column: "WinnerRes"}, data.resources[0]), 
-                        React.createElement(Td, {column: "Looser"}, hrefs[1]), 
-                        React.createElement(Td, {column: "LooserRes"}, data.resources[1]), 
-                        React.createElement(Td, {column: "Replay"}, 
-                            React.createElement("a", {href: `https://codeit.itdhosting.de:1337/replay.php?game=${id}`}, "Show Replay")
-                        ));
-                }
-            };
-            defaultState = { displayLimit: 200, mapFilter: [], playerFilter: [], winnerFilter: null, looserFilter: null };
+            idToName = new Map();
             GUI = class GUI extends React.Component {
                 constructor(props) {
                     super(props);
                     const data = props.data;
-                    this.maps = getUnique(data, "map").map(map => ({ value: map, label: map }));
+                    this.maps = [...new Set(Object.keys(data).map(k => data[k]).map(game => game.map))].map(map => ({ value: map, label: map }));
                     this.players = users.map(user => ({ value: user.id, label: user.name }));
-                    this.state = defaultState;
+                    this.state = deserializeState(location.search);
                     this.ids = Object.keys(data).map(x => +x).sort((a, b) => b - a);
+                    const mulSize = (a) => a.mapSize.split("x").reduce((a, b) => +a * +b, 1);
+                    const numberCompare = (a, b) => a - b;
+                    const localeCompare = (a, b) => a.localeCompare(b);
+                    function comparing(extractor, comparator) {
+                        return (a1, a2) => comparator(extractor(data[a1]), extractor(data[a2]));
+                    }
+                    this.sorters = {
+                        ID: numberCompare,
+                        Date: numberCompare,
+                        Map: comparing(x => x.map, localeCompare),
+                        "Map Size": comparing(a => a.mapSize.split("x").reduce((a, b) => +a * +b, 1), numberCompare),
+                        "Winner": comparing(x => idToName.get(x.ranking[0]), localeCompare),
+                        "Winner Resources": comparing(x => x.resources[0], numberCompare),
+                        "Looser": comparing(x => idToName.get(x.ranking[1]), localeCompare),
+                        "Looser Resources": comparing(x => x.resources[1], numberCompare),
+                        "Replay": numberCompare
+                    };
                 }
                 componentWillUpdate(props, newState) {
                     if (this.state.displayLimit == newState.displayLimit)
-                        newState.displayLimit = defaultState.displayLimit;
+                        newState.displayLimit = deserializeState("").displayLimit;
+                }
+                setSort(event, sortColumn) {
+                    event.preventDefault();
+                    if (this.state.sortColumn == sortColumn)
+                        this.setState({ sortReverse: !this.state.sortReverse });
+                    else
+                        this.setState({ sortColumn });
                 }
                 render() {
                     const data = this.props.data;
-                    const { mapFilter, playerFilter, winnerFilter, looserFilter, displayLimit } = this.state;
+                    const { mapFilter, playerFilter, winnerFilter, looserFilter, displayLimit, sortColumn, sortReverse } = this.state;
                     let ids = [];
                     for (const id of this.ids) {
                         const game = data[id];
                         if ((!mapFilter || mapFilter.length == 0 || mapFilter.some(selectedMap => game.map === selectedMap.value))
                             && (!playerFilter || playerFilter.length == 0 || playerFilter.every(player => game.ranking.indexOf(+player.value) >= 0))
                             && (!winnerFilter || game.ranking[0] === winnerFilter.value)
-                            && (!looserFilter || game.ranking[1] === looserFilter.value)) {
+                            && (!looserFilter || game.ranking[1] === looserFilter.value))
                             ids.push(id);
-                        }
                     }
-                    length = ids.length;
+                    const count = ids.length;
+                    if (this.state.sortColumn) {
+                        let sorter = this.sorters[this.state.sortColumn];
+                        if (this.state.sortReverse)
+                            ids.sort((a, b) => -sorter(a, b));
+                        else
+                            ids.sort(sorter);
+                    }
+                    ids = ids.slice(0, displayLimit);
                     return React.createElement("div", null, 
                         React.createElement("div", {className: "row"}, 
                             React.createElement("div", {className: "col-sm-3"}, 
@@ -120,21 +134,23 @@ System.register(['react', 'react-dom', 'react-select', 'query-string', "reactabl
                         React.createElement("hr", null), 
                         React.createElement("div", {className: "alert alert-info"}, 
                             "Found ", 
-                            length, 
+                            count, 
                             " games"), 
-                        React.createElement(Table, {className: "table", id: "table", itemsPerPage: 40, pageButtonLimit: 10, sortable: true}, 
-                            React.createElement(Thead, null, 
-                                React.createElement(Th, {column: "ID"}, "ID"), 
-                                React.createElement(Th, {column: "Date"}, "Date"), 
-                                React.createElement(Th, {column: "Map"}, "Map"), 
-                                React.createElement(Th, {column: "Map Size"}, "Map Size"), 
-                                React.createElement(Th, {column: "Winner"}, "Winner"), 
-                                React.createElement(Th, {column: "WinnerRes"}, "Winner Resources"), 
-                                React.createElement(Th, {column: "Looser"}, "Looser"), 
-                                React.createElement(Th, {column: "LooserRes"}, "Looser Resources"), 
-                                React.createElement(Th, {column: "Replay"}, "Replay")), 
-                            ids.map(id => GameRender.get(data[id], id))), 
-                        ",");
+                        React.createElement("table", {className: "table", data: ids}, 
+                            React.createElement("thead", null, 
+                                React.createElement("tr", null, "ID,Date,Map,Map Size,Winner,Winner Resources,Looser,Looser Resources,Replay".split(",").map(name => React.createElement("th", {key: name}, 
+                                    React.createElement("a", {href: "#", onClick: e => this.setSort(e, name)}, 
+                                        name, 
+                                        this.state.sortColumn === name ? this.state.sortReverse ? "▼" : "▲" : "")
+                                )))
+                            ), 
+                            React.createElement("tbody", null, ids.map(id => renderTableRow(data[id], id)))), 
+                        count > displayLimit ?
+                            React.createElement("div", {className: "alert alert-danger"}, 
+                                count - displayLimit, 
+                                " more not displayed.", 
+                                " ", 
+                                React.createElement("a", {href: "#", onClick: e => { e.preventDefault(); this.setState({ displayLimit: displayLimit * 2 }); }}, "Show more")) : "");
                 }
                 componentDidUpdate() {
                     history.replaceState(null, null, "?" + serializeState(this.state));
@@ -145,12 +161,9 @@ System.register(['react', 'react-dom', 'react-select', 'query-string', "reactabl
                 .then(jsonOrError)
                 .then((data) => {
                 users = data;
-                for (const { id, name } of data) {
+                for (const { id, name } of users)
                     idToName.set(id, name);
-                    nameToId.set(name, id);
-                }
             })
-                .then(() => defaultState = deserializeState(location.search))
                 .then(() => fetch("../data/games.json"))
                 .then(jsonOrError)
                 .then((data) => render(React.createElement(GUI, {data: data})))
